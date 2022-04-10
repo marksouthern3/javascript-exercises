973abcda612d83c63cca0ed2ceb63b5268a92d9d
Mark Southern <marksouthern3@gmail.com>
2022-04-10 14:29:59 +0100

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(inputArray, ...args) {
+    for (let i = inputArray.length - 1; i >= 0; i--) {
+        for (const arg of args) {
+            if (inputArray[i] === arg) {
+                inputArray.splice(i, 1);
+                break;
+            }
+        }
+    }
+    return inputArray;
 };
 
 // Do not edit below this line

a0f2550cc0cd52eb688af8de78556eabbe11a0ac
Mark Southern <marksouthern3@gmail.com>
2022-04-10 14:09:19 +0100

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return "ERROR";
+    let outputString = "";
+    for (let i = 1; i <= num; i++) {
+        outputString += string;
+    }
+    return outputString;
 };
 
 // Do not edit below this line

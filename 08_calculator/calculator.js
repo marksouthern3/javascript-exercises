f2db5018e6ed100b755d1d134fd31ed18caeda39
Mark Southern <marksouthern3@gmail.com>
2022-04-17 14:28:20 +0100

    Complete exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let summ = 0;
+  arr.forEach(num => summ += num);
+  return summ;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let product = 1;
+  arr.forEach(num => product *= num);
+  return product;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  result = 1;
+  for (let i = num; i > 0; i--) result *= i;
+  return result;
 };
 
 // Do not edit below this line

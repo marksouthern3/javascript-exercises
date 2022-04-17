05b22e8732bdad73f2804011f16b55b232842742
Mark Southern <marksouthern3@gmail.com>
2022-04-17 14:41:38 +0100

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string.replace(/[^\w\s]|_/g, "");  // remove punctuation
+    string = string.replaceAll(' ', '');  // remove white space
+    string = string.toLowerCase();  // remove capitalisation
+    reverseString = '';
+    for (let i = string.length - 1; i >= 0; i--) {
+        reverseString += string.charAt(i);
+    }
+    return reverseString === string;
 };
 
 // Do not edit below this line

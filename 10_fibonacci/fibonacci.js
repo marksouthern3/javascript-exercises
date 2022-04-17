0254efba93dee89e3f23753a24bce66b634a9ca0
Mark Southern <marksouthern3@gmail.com>
2022-04-17 14:51:34 +0100

    Complete exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
-
+const fibonacci = function(number) {
+    number = parseInt(number);
+    if (number < 1) return 'OOPS';
+    if (number === 1 || number === 2) return 1;
+    previous = 1;
+    current = 1;
+    for (let i = 3; i <= number; i++) {
+        let tempCurrent = current;
+        current = previous + current;
+        previous = tempCurrent;
+    }
+    return current;
 };
 
 // Do not edit below this line

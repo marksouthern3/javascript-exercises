e722b22adcb3d46caa2deb7e3cb6cba07a5a4dd0
Mark Southern <marksouthern3@gmail.com>
2022-04-17 15:39:06 +0100

    Finish exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldestPerson, currentPerson) => {
+        let currentDeath = currentPerson.yearOfDeath;
+        if (!currentDeath) currentDeath = new Date().getFullYear();
+        let oldestDeath = oldestPerson.yearOfDeath;
+        if (!oldestDeath) oldestDeath = new Date().getFullYear();
+        let currentPersonAge = currentDeath - currentPerson.yearOfBirth;
+        let oldestPersonAge = oldestDeath - oldestPerson.yearOfBirth;
+        if (currentPersonAge > oldestPersonAge) return currentPerson;
+        return oldestPerson;
+    });
 };
 
 // Do not edit below this line
